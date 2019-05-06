b3e683092a2a7b22d109b8af9d9c34e9c63bcab2
unknown <sara.fellner@gmail.com>
2019-05-06 11:38:46 +0200

    Added Forms

---
+++ Self-Organizing Map/Form/MainForm.Designer.cs
@@ -0,0 +1,88 @@
+﻿namespace Self_Organizing_Map
+{
+    partial class MainForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.colorButton = new System.Windows.Forms.Button();
+            this.flowerButton = new System.Windows.Forms.Button();
+            this.wordButton = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // colorButton
+            // 
+            this.colorButton.Location = new System.Drawing.Point(85, 35);
+            this.colorButton.Name = "colorButton";
+            this.colorButton.Size = new System.Drawing.Size(75, 23);
+            this.colorButton.TabIndex = 0;
+            this.colorButton.Text = "Color SOM";
+            this.colorButton.UseVisualStyleBackColor = true;
+            this.colorButton.Click += new System.EventHandler(this.ColorButton_Click);
+            // 
+            // flowerButton
+            // 
+            this.flowerButton.Location = new System.Drawing.Point(85, 81);
+            this.flowerButton.Name = "flowerButton";
+            this.flowerButton.Size = new System.Drawing.Size(75, 23);
+            this.flowerButton.TabIndex = 1;
+            this.flowerButton.Text = "Flower SOM";
+            this.flowerButton.UseVisualStyleBackColor = true;
+            this.flowerButton.Click += new System.EventHandler(this.FlowerButton_Click);
+            // 
+            // wordButton
+            // 
+            this.wordButton.Location = new System.Drawing.Point(85, 127);
+            this.wordButton.Name = "wordButton";
+            this.wordButton.Size = new System.Drawing.Size(75, 23);
+            this.wordButton.TabIndex = 2;
+            this.wordButton.Text = "Word SOM";
+            this.wordButton.UseVisualStyleBackColor = true;
+            this.wordButton.Click += new System.EventHandler(this.WordButton_Click);
+            // 
+            // MainForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(245, 185);
+            this.Controls.Add(this.wordButton);
+            this.Controls.Add(this.flowerButton);
+            this.Controls.Add(this.colorButton);
+            this.Name = "MainForm";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "SOM";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button colorButton;
+        private System.Windows.Forms.Button flowerButton;
+        private System.Windows.Forms.Button wordButton;
+    }
+}
+
---
+++ Self-Organizing Map/Form/MainForm.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Self_Organizing_Map
+{
+    public partial class MainForm : Form
+    {
+        public MainForm()
+        {
+            InitializeComponent();
+        }
+
+        private void ColorButton_Click(object sender, EventArgs e)
+        {
+            ColorForm colorForm = new ColorForm();
+            colorForm.Show();
+        }
+
+        private void FlowerButton_Click(object sender, EventArgs e)
+        {
+            FlowerForm flowerForm = new FlowerForm();
+            flowerForm.Show();
+        }
+
+        private void WordButton_Click(object sender, EventArgs e)
+        {
+            WordForm wordForm = new WordForm();
+            wordForm.Show();
+        }
+    }
+}
