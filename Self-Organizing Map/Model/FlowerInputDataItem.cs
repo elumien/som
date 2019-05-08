453b7dff330a3f1bd688f86e4c3e5be3491ead7f
unknown <sara.fellner@gmail.com>
2019-05-08 12:25:41 +0200

    Added Flowe input classes

---
+++ Self-Organizing Map/Model/FlowerInputDataItem.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MathNet.Numerics.LinearAlgebra;
+
+namespace Self_Organizing_Map.Model
+{
+    class FlowerInputDataItem : InputDataItem
+    {
+        public FlowerInputDataItem(Vector<double> inputVector) : base(inputVector)
+        {
+        }
+
+        public void csinajjad()
+        {
+
+            using (var reader = new StreamReader("Resource/iris.cvs"))
+            {
+                List<string> listA = new List<string>();
+                List<string> listB = new List<string>();
+                while (!reader.EndOfStream)
+                {
+                    var line = reader.ReadLine();
+                    var values = line.Split(';');
+
+                    listA.Add(values[0]);
+                    listB.Add(values[1]);
+                }
+            }
+        }
+
+    }
+}
---
+++ Self-Organizing Map/Model/FlowerInputDataSet.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Self_Organizing_Map.Model
+{
+    class FlowerInputDataSet : InputDataSet
+    {
+        public FlowerInputDataSet(List<InputDataItem> inputDataItems, int inputVectorDimension, int inputDataItemNumber) : base(inputDataItems, inputVectorDimension, inputDataItemNumber)
+        {
+
+        }
+
+        public void ReadCVS()
+        {
+
+            using (var reader = new StreamReader("Resource/iris.cvs"))
+            {
+                List<string> listA = new List<string>();
+                List<string> listB = new List<string>();
+                while (!reader.EndOfStream)
+                {
+                    var line = reader.ReadLine();
+                    var values = line.Split(';');
+
+                    listA.Add(values[0]);
+                    listB.Add(values[1]);
+                }
+            }
+        }
+    }
+}
